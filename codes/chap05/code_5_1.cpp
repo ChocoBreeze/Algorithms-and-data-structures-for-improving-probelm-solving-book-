d75751084cee751ed769c866a784fa69117ad88a
ChocoBreeze <75352656+ChocoBreeze@users.noreply.github.com>
2022-04-18 00:18:51 +0900

    Update code_5_1.cpp

--- codes/chap05/code_5_1.cpp
+++ codes/chap05/code_5_1.cpp
@@ -1,27 +1,29 @@
+// ê°œêµ¬ë¦¬ ë¬¸ì œ
+// ë§í¬ : https://atcoder.jp/contests/dp/tasks/dp_a
+// ië²ˆì§¸ë¡œ ê°€ëŠ” ë°©ë²•ì€ i-1ë²ˆì§¸ì—ì„œ jumpí•˜ê±°ë‚˜ i-2ë²ˆì¬ì—ì„œ jump --> ë‘˜ ì¤‘ min
+
 #include <iostream>
 #include <vector>
 using namespace std;
-const long long INF = 1LL << 60; // ½½Ê¬Âç¤­¤¤ÃÍ¤È¤¹¤ë (¤³¤³¤Ç¤Ï 2^60)
+const long long INF = 1LL << 60; // ì¶©ë¶„íˆ í° ê°’ìœ¼ë¡œ ì´ˆê¸°í™”
 
 int main() {
-    // ÆşÎÏ
     int N; cin >> N;
     vector<long long> h(N);
     for (int i = 0; i < N; ++i) cin >> h[i];
 
-    // ÇÛÎó dp ¤òÄêµÁ (ÇÛÎóÁ´ÂÎ¤òÌµ¸ÂÂç¤òÉ½¤¹ÃÍ¤Ë½é´ü²½)
+    // ë°°ì—´ dp ì •ì˜
     vector<long long> dp(N, INF);
 
-    // ½é´ü¾ò·ï
+    // ì´ˆê¸° ì¡°ê±´
     dp[0] = 0;
 
-    // ¥ë¡¼¥×
+    // ë£¨í”„
     for (int i = 1; i < N; ++i) {
         if (i == 1) dp[i] = abs(h[i] - h[i - 1]);
         else dp[i] = min(dp[i - 1] + abs(h[i] - h[i - 1]), 
                          dp[i - 2] + abs(h[i] - h[i - 2]));
     }
 
-    // Åú¤¨
     cout << dp[N - 1] << endl;
 }
