3b7a92d449a2f1a27fb02dc111919a738783d817
ChocoBreeze <75352656+ChocoBreeze@users.noreply.github.com>
2022-04-21 01:11:45 +0900

    Update code_7_3.cpp

--- codes/chap07/code_7_3.cpp
+++ codes/chap07/code_7_3.cpp
@@ -1,18 +1,20 @@
+// ë¬¸ì œ ë§í¬ https://atcoder.jp/contests/agc009/tasks/agc009_a
+// O(N)
+// ìƒê°í•˜ì!
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main() {
-    // ÆşÎÏ
     int N;
     cin >> N;
     vector<long long> A(N), B(N);
     for (int i = 0; i < N; ++i) cin >> A[i] >> B[i];
 
-    // Åú¤¨
+    // ë‹µ
     long long sum = 0;
     for (int i = N - 1; i >= 0; --i) {
-        A[i] += sum; // Á°²ó¤Ş¤Ç¤ÎÁàºî²ó¿ô¤òÂ­¤¹
+        A[i] += sum; // ì „íšŒê¹Œì§€ì˜ ì‘ì—… íšŸìˆ˜ ë”í•˜ê¸°
         long long amari = A[i] % B[i];
         long long D = 0;
         if (amari != 0) D = B[i] - amari;
