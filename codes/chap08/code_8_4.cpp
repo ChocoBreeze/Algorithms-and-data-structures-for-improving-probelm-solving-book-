a3f034a3ace228bca58fbc3eda59e1d1cf3d3ca7
ChocoBreeze <75352656+ChocoBreeze@users.noreply.github.com>
2022-04-21 23:46:33 +0900

    Update code_8_4.cpp

--- codes/chap08/code_8_4.cpp
+++ codes/chap08/code_8_4.cpp
@@ -3,62 +3,64 @@
 #include <vector>
 using namespace std;
 
-// Ϣ��ꥹ�ȤγƥΡ��ɤ�ɽ����¤��
+// 연결 리스트의 각 노드를 가리키는 구조체
 struct Node {
-    Node* next; // �����ɤΥΡ��ɤ�ؤ���
-    string name; // �Ρ��ɤ��տ路�Ƥ�����
+    Node* next; // 다음 노드 지정
+    string name; // 노드에 저장하는 값
 
     Node(string name_ = "") : next(NULL), name(name_) { }
 };
 
-// ��ʼ��ɽ���Ρ��ɤ򥰥����Х��ΰ���֤��Ƥ���
+// 감시 노드를 전역 변수로 지정
 Node* nil;
 
-// �����
+// 초기화
 void init() {
     nil = new Node();
-    nil->next = nil; // ������֤Ǥ� nil �� nil ��ؤ��褦�ˤ���
+    nil->next = nil; // 초기 상태는 nil이 nil을 가리킴
+    // nil : sentinel node의 역할
 }
 
-// Ϣ��ꥹ�Ȥ���Ϥ���
+// 연결 리스트 출력
 void printList() {
-    Node* cur = nil->next; // ��Ƭ�����ȯ
+    Node* cur = nil->next; // 선두부터 출발
     for (; cur != nil; cur = cur->next) {
         cout << cur->name << " -> ";
     }
     cout << endl;
 }
 
-// �Ρ��� p ��ľ��˥Ρ��� v ����������
-// �Ρ��� p �Υǥե���Ȱ����� nil �Ȥ��Ƥ���
-// ���Τ��� insert(v) ��ƤӽФ����ϡ��ꥹ�Ȥ���Ƭ�ؤ�������ɽ��
+// 노드 p 직후에 노드 v를 삽입
+// 노드 p 기본 인수를 nil로 지정
+// 따라서 insert(v)를 호출하면 리스트 선두에 삽입이 됨
 void insert(Node* v, Node* p = nil) {
     v->next = p->next;
     p->next = v;
 }
 
 int main() {
-    // �����
+    // 초기화
     init();
 
-    // ��ꤿ���Ρ��ɤ�̾���ΰ���
-    // �Ǹ����ΥΡ��� (�ֻ��ܡ�) �������������뤳�Ȥ�����
-    vector<string> names = {"yamamoto",
-                            "watanabe",
-                            "ito",
-                            "takahashi",
-                            "suzuki",
-                            "sato"};
+    // 만들고 싶은 노드명 목록
+    // 마지막 노드인(백호)부터 삽입이 됨.
+    vector<string> names = {"bakho",
+                            "myongjin",
+                            "rangi",
+                            "daeho",
+                            "nara",
+                            "gildong"};
+    // 최종 순서 nil -> 길동 -> 나라 -> 대호 -> 랑이 -> 명진 -> 백호 -> nil
 
-    // �ƥΡ��ɤ��������ơ�Ϣ��ꥹ�Ȥ���Ƭ���������Ƥ���
+    // 각 노드를 생성하고 연결 리스트 선두에 삽입함
     for (int i = 0; i < (int)names.size(); ++i) {
-        // �Ρ��ɤ��������
+        // 노드 작성
         Node* node = new Node(names[i]);
 
-        // ���������Ρ��ɤ�Ϣ��ꥹ�Ȥ���Ƭ����������
+        // 작성한 노드를 연결 리스트 선두에 삽입
         insert(node);
 
-        // �ƥ��ƥåפ�Ϣ��ꥹ�Ȥ��ͻҤ���Ϥ���
+        // 각 단계의 연결 리스트 상황을 출력
         cout << "step " << i << ": ";
         printList();
     }
