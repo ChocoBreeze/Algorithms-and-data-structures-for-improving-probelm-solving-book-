bf898377c557d04cda1fc3cc4404bffbf1839964
ChocoBreeze <75352656+ChocoBreeze@users.noreply.github.com>
2022-05-05 13:26:22 +0900

    Update code_2_4.cpp

--- codes/chap02/code_2_4.cpp
+++ codes/chap02/code_2_4.cpp
@@ -3,33 +3,33 @@
 #include <cmath>
 using namespace std;
 
-// 2 �� (x1, y1) �� (x2, y2) �Ȥε�Υ�����ؿ�
+// 두 점 (x1, y1)과 (x2, y2) 사이 거리를 구하는 함수
 double calc_dist(double x1, double y1, double x2, double y2) {
     return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 }
 
 int main() {
-    // ���ϥǡ�����������
+    // 데이터를 입력 받음
     int N; cin >> N;
     vector<double> x(N), y(N);
     for (int i = 0; i < N; ++i) cin >> x[i] >> y[i];
-    
-    // �����ͤ򡢽�ʬ�礭���ͤǽ�������Ƥ���
+
+    // 결과 값이 들어갈 변수를 초기화(결과 값 후보보다 훨씬 큰 값)
     double minimum_dist = 100000000.0;
-    
-    // õ������
+
+    // 탐색 시작
     for (int i = 0; i < N; ++i) {
         for (int j = i + 1; j < N; ++j) {
-            // (x[i], y[i]) �� (x[j], y[j]) �Ȥε�Υ
+            // (x[i], y[i])와 (x[j], y[j]) 사이 거리
             double dist_i_j = calc_dist(x[i], y[i], x[j], y[j]);
-            
-            // ����Ǿ��� minimum_dist �� dist_i_j ����٤�
+
+            // 잠정 최솟값 minimum_dist를 dist_i_j와 비교
             if (dist_i_j < minimum_dist) {
                 minimum_dist = dist_i_j;
             }
         }
     }
-    
-    // ��������Ϥ���
+
+    // 결과 출력
     cout << minimum_dist << endl;
 }
