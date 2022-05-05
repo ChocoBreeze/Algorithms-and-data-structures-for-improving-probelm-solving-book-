f4ef1b9cc02ae6b866717e3e35318c62ce4c74b8
ChocoBreeze <75352656+ChocoBreeze@users.noreply.github.com>
2022-05-05 15:20:10 +0900

    Update code_14_1.cpp

--- codes/chap14/code_14_1.cpp
+++ codes/chap14/code_14_1.cpp
@@ -1,3 +1,5 @@
+// 완화
+// 갱신 여부를 bool 값으로 return 
 template<class T> bool chmin(T& a, T b) {
     if (a > b) {
         a = b;
@@ -5,3 +7,7 @@ template<class T> bool chmin(T& a, T b) {
     }
     else return false;
 }
+
+// e = (u, v)에 대한 완화 처리
+// chmin(d[v], d[u] + l{e))
+// d[v]의 초기값은 v=s일 때, 0 // v!=s일 때 infinity
